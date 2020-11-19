d87690a1e48a182cd7a2d7657ae54c10dff7227c
Dario Garcia <darcius_95@hotmail.com>
2020-11-19 20:10:23 +0100

    Creada la redireccion en el boton de cerrar para que nos lleve al home. Creado un guard para que si la intro ya esta vista no vuelva a salir

--- platzi-music/src/app/home/home.page.ts
+++ platzi-music/src/app/home/home.page.ts
@@ -5,13 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  slideOpt = {
-    initialSlide: 0,
-    slidesperView: 1,
-    centeredSlides: true,
-    speed: 400,
-  };
+  
   
   constructor() {}
 
---
+++ platzi-music/src/app/intro/intro.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from "@ionic/storage";
+
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+  slideOpt = {
+    initialSlide: 0,
+    slidesperView: 1,
+    centeredSlides: true,
+    speed: 400,
+  };
+
+  constructor(private router: Router, private storage: Storage) { }
+
+  finish(){
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl("/home");
+  }
+
+  ngOnInit() {
+  }
+
+}
