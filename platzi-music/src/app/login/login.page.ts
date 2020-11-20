8b892e0a9d46c81acb11da741eeae2acf4324217
Dario Garcia <darcius_95@hotmail.com>
2020-11-20 19:19:01 +0100

    Primer paso para crear la pagina del login. Se crea y declara lo necesario acerca del formulario y se crea un input email

---
+++ platzi-music/src/app/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+      ])
+      )
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
