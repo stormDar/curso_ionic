84ee467d11a2b035ec25810ac6dc8d9fb6cfbe96
Dario Garcia <darcius_95@hotmail.com>
2020-11-23 20:38:39 +0100

    Añadida la validacion de los inputs mostrando mensaje en caso de que el input sea erroneo

--- platzi-music/src/app/login/login.page.ts
+++ platzi-music/src/app/login/login.page.ts
@@ -9,6 +9,16 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
+  validation_message = {
+    email: [
+      { type: "required", message: " El email es requerido" },
+      { type: "pattern", message: "ojo! este no es un email válido" }
+    ],
+    password: [
+      { type: "required", message: " El password es requerido" },
+      { type: "minlength", message: "Minimo 5 letras para el password" }
+    ]
+  };
 
   constructor(private formBuilder: FormBuilder) { 
     this.loginForm = this.formBuilder.group({
@@ -16,7 +26,14 @@ export class LoginPage implements OnInit {
         Validators.required,
         Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
       ])
+      ),
+
+      password: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])
       )
+
     });
   }
 
