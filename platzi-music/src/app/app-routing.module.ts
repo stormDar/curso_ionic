97dc8536b3dec8bb6d28a546a70c4e6550bb8e7c
Dario Garcia <darcius_95@hotmail.com>
2020-11-27 18:47:49 +0100

    Agrego componente menu con alguna opcion, aun sin funcionar

--- platzi-music/src/app/app-routing.module.ts
+++ platzi-music/src/app/app-routing.module.ts
@@ -6,11 +6,6 @@ import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    canActivate:[LoginGuard, IntroGuard]
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
@@ -22,9 +17,15 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule),
+    canActivate:[LoginGuard, IntroGuard]
   },
 
 
